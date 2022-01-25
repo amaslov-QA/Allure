112bcff5fe299de2344fe35b0bf4704a2bc994a7
aa_maslov <smart.maslov@gmail.com>
2022-01-25 12:14:20 +0300

    add file

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,133 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class CardDeliveryTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planningDate = generateDate(5);
+
+    @Test
+    public void shouldCheckingTheCorrectForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("+79112650042");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='notification']").shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible).
+                shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+
+    }
+    @Test
+    public void shouldCheckingTheFormNoPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+
+    }
+    @Test
+    public void shouldCheckingTheFormNoCity() {
+        open("http://localhost:9999/");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("+79112650042");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='city'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+
+    }
+    @Test
+    public void shouldCheckingTheFormNoCheckbox() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("+79112650042");
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='agreement'] .checkbox__text").shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+
+    }
+    @Test
+    public void shouldCheckingTheFormMoreNumberPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("+791126500428");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").
+                shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+    @Test
+    public void shouldCheckingTheFormPhonePlus() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("79112650042+");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").
+                shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }    @Test
+    public void shouldCheckingTheFormNoAdministrativeCenter() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Бугульма");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Аркадий Паровозов");
+        $("[data-test-id='phone'] input").setValue("+79112650042");
+        $("[data-test-id='agreement']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id='city'].input_invalid .input__sub").
+                shouldHave(Condition.text("Доставка в выбранный город недоступна"));
+
+    }
+
+
+}
+
+
+
